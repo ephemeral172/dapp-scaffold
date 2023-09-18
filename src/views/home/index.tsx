e0cc6fabe9a711976006d4c3c4816c5f7aad17fb
ephemeral172 <145383794+ephemeral172@users.noreply.github.com>
2023-09-18 21:26:32 +0300

    Update index.tsx

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -34,7 +34,8 @@ export const HomeView: FC = ({ }) => {
     <div className="md:hero mx-auto p-4">
       <div className="md:hero-content flex flex-col">
         <div className='mt-6'>
-
+<button 
+  onClick={() =>
 window.Jupiter.init({
     "displayMode": "integrated",
     "integratedTargetId": "integrated-terminal",
@@ -45,8 +46,11 @@ window.Jupiter.init({
         "initialAmount": "8888888800000",
         "initialInputMint": "DezXAZ8z7PnrnRJjz3wXBoRgixCa6xjnB7YaB1pPB263",
         "initialOutputMint": "AZsHEMXd36Bj1EMNXhowJajpUXzrKcK57wW4ZGXVa7yR"
-    }
-});
+    },
+})
+  }
+  >
+</button>
           
         <div className='text-sm font-normal align-bottom text-right text-slate-600 mt-4'>v{pkg.version}</div>
         <h1 className="text-center text-5xl md:pl-12 font-bold text-transparent bg-clip-text bg-gradient-to-br from-indigo-500 to-fuchsia-500 mb-4">
