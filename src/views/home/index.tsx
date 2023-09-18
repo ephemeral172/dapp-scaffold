0db02da9a51963c299769443f9f9ac6968fe76ff
ephemeral172 <145383794+ephemeral172@users.noreply.github.com>
2023-09-18 21:34:19 +0300

    Update index.tsx

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -34,22 +34,7 @@ export const HomeView: FC = ({ }) => {
     <div className="md:hero mx-auto p-4">
       <div className="md:hero-content flex flex-col">
         <div className='mt-6'>
-<button 
-  onClick={() =>
-window.Jupiter.init({
-    "displayMode": "integrated",
-    "integratedTargetId": "integrated-terminal",
-    "endpoint": "https://api.mainnet-beta.solana.com",
-    "strictTokenList": false,
-    "formProps": {
-        "fixedOutputMint": true,
-        "initialAmount": "8888888800000",
-        "initialInputMint": "DezXAZ8z7PnrnRJjz3wXBoRgixCa6xjnB7YaB1pPB263",
-        "initialOutputMint": "AZsHEMXd36Bj1EMNXhowJajpUXzrKcK57wW4ZGXVa7yR"
-    },
-})
-  }
-  ></button>
+
 
           
         <div className='text-sm font-normal align-bottom text-right text-slate-600 mt-4'>v{pkg.version}</div>
